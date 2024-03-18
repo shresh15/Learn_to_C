efeff0019c3d3255430c8a111bedd1b5340ec65b
shrestha <shresthachakrabortykgp@gmail.com>
2024-03-18 19:08:36 +0530

    new course code

---
+++ Codechef/Courses/LBCL18_problems_CLB086.c
@@ -0,0 +1,12 @@
+// Write a program which does the following
+
+// Create an Array of the first 5 positive integers
+// Once the array is define - output "Done" to the console.
+#include <stdio.h>
+
+int main(void) {
+	int numbers[5] = {1, 2, 3, 4, 5};
+	printf("Done");
+
+}
+
