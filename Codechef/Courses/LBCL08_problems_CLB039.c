2d73ea7a9ad2036da74326136441e0b4efba8db5
shrestha <shresthachakrabortykgp@gmail.com>
2024-03-19 00:04:48 +0530

    ADDED

---
+++ Codechef/Courses/LBCL08_problems_CLB039.c
@@ -0,0 +1,23 @@
+// Write a program which does the following
+
+// Declare two strings x and y
+// Assign the values "Hello" to variable x and "World" to variable y.
+// Concatenate the two string and print the output.
+#include <stdio.h>
+#include <string.h>
+ 
+int main () {
+
+    char x[] = "Hello";
+    char y[]="World";
+    
+    // declare string y with "World"
+    
+    // concatenate the two strings
+    
+    // print the final string
+    printf("%s", x);
+    printf("%s", y);
+    
+
+}
---
+++ Codechef/Courses/LBCL08_problems_CLB042.c
@@ -0,0 +1,13 @@
+// Write a program which does the following
+
+// Create a string variable word and assign the text "Programming" to it
+// Print the characters o and r from the string word in separate lines using the syntax defined above
+#include <stdio.h>
+
+int main() {
+
+    char word[] = "Programming";
+    printf("%c\n", word[ 2 ]);
+    printf("%c", word[ 1 ]);
+
+}
